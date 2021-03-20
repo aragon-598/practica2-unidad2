c9daf33a5349a23a796f7444625788cbf5c9fb8f
Alejandro Aragón <aragondru155@gmail.com>
2021-03-20 11:56:20 -0600

    initial commit

---
+++ VideoClub/src/main/java/ues/occ/edu/sv/ingenieria/tpi135/videoclub/ClientVideoClubService.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ues.occ.edu.sv.ingenieria.tpi135.videoclub;
+
+/**
+ *
+ * @author aragon598
+ */
+public class ClientVideoClubService {
+    
+}
